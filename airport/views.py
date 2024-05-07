7e23de9fee2e858a8d38ebf70d202fab2db7a4da
MaksymUK <clear>
2024-05-07 07:23:24 +0100

    implement basic views for all models

--- airport/views.py
+++ airport/views.py
@@ -23,8 +23,7 @@ from .serializers import (
 
 
 class AirplaneTypeViewSet(
-    mixins.ListModelMixin,
-    GenericViewSet
+    viewsets.ModelViewSet
 ):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
@@ -84,11 +83,4 @@ class OrderViewSet(
 ):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
 
