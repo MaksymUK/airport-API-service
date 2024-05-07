45bd8f26765ab0de1cc50a3aec9730209924409b
MaksymUK <clear>
2024-05-07 07:20:14 +0100

    add TicketInLine class to admin.py

--- airport/admin.py
+++ airport/admin.py
@@ -11,11 +11,21 @@ from .models import (
     Ticket,
 )
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(AirplaneType)
 admin.site.register(Airplane)
 admin.site.register(Airport)
 admin.site.register(Route)
 admin.site.register(Crew)
 admin.site.register(Flight)
-admin.site.register(Order)
 admin.site.register(Ticket)
